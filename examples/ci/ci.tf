6a327466ac679a8d9f4b0f8cb3fee74c1b5310ba
Josh Gubler <josh.gubler@gmail.com>
2022-03-09 15:32:13 -0800

    document usage

--- examples/ci/ci.tf
+++ examples/ci/ci.tf
@@ -1,12 +1,20 @@
 terraform {
-  required_version = ">= 0.12.0"
+  required_version = "1.0.0"
 }
 
 provider "aws" {
-  version = "~> 2.42"
+  version = "~> 3.0"
   region  = "us-west-2"
 }
 
 module "ci_test" {
   source = "../../"
+}
+
+output "creation_policy_arn" {
+  value = module.ci_test.creation_policy.arn
+}
+
+output "use_policy_arn" {
+  value = module.ci_test.use_policy.arn
 }
--- examples/simple/simple-example.tf
+++ examples/simple/simple-example.tf
@@ -1,9 +1,32 @@
 provider "aws" {
-  version = "~> 2.42"
+  version = "~> 3.0"
   region  = "us-west-2"
 }
 
-module "<module_name>" {
-  source = "github.com/byu-oit/terraform-aws-<module_name>?ref=v1.0.0"
-  #source = "../" # for local testing during module development
+locals {
+  name = "my_project"
+  env  = "dev"
+  tags = {
+    env              = local.env
+    data-sensitivity = "public"
+    repo             = "https://github.com/byu-oit/${local.name}"
+  }
+}
+
+module "backend-s3-cicd" {
+  source = "github.com/byu-oit/terraform-aws-backend-s3-cicd?ref=v1.0.0"
+}
+
+module "gha_role" {
+  source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                        = "4.13.2"
+  create_role                    = true
+  role_name                      = "${local.name}-${var.env}-gha"
+  role_permissions_boundary_arn  = module.acs.role_permissions_boundary.arn
+  tags                           = local.tags
+  provider_url                   = "token.actions.githubusercontent.com"
+  oidc_fully_qualified_audiences = ["sts.amazonaws.com"]
+  oidc_subjects_with_wildcards   = ["repo:byu-oit/${local.name}:*"]
+  number_of_role_policy_arns     = 1
+  role_policy_arns               = [module.backend-s3-cicd.use_policy.arn]
 }
